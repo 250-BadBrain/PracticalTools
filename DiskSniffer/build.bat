9092dff085cc4f35b9a5af1aaa78bbba9c3d914e
250-BadBrain <1445355752@qq.com>
2026-06-06 10:58:59 +0800

    fix: 修改构建脚本，删除命名冗余

--- DiskSniffer/build.bat
+++ DiskSniffer/build.bat
@@ -5,14 +5,14 @@ if not exist dist mkdir dist
 
 where cl >nul 2>nul
 if %errorlevel%==0 (
-  cl /nologo /O2 /EHsc /DUNICODE /D_UNICODE disksniffer.cpp /link /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /OUT:dist\disksniffer-windows-amd64.exe Comctl32.lib Shell32.lib Ole32.lib Uuid.lib
+  cl /nologo /O2 /EHsc /DUNICODE /D_UNICODE disksniffer.cpp /link /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /OUT:dist\disksniffer.exe Comctl32.lib Shell32.lib Ole32.lib Uuid.lib
   if errorlevel 1 exit /b 1
   exit /b 0
 )
 
 where g++ >nul 2>nul
 if %errorlevel%==0 (
-  g++ -std=c++17 -O2 -ffunction-sections -fdata-sections -municode -mwindows disksniffer.cpp -Wl,--gc-sections -s -static -lcomctl32 -lshell32 -lole32 -luuid -o dist\disksniffer-windows-amd64.exe
+  g++ -std=c++17 -O2 -ffunction-sections -fdata-sections -municode -mwindows disksniffer.cpp -Wl,--gc-sections -s -static -lcomctl32 -lshell32 -lole32 -luuid -o dist\disksniffer.exe
   if errorlevel 1 exit /b 1
   exit /b 0
 )
